ccda40e1f7ebcb5d986c64724789c1db5f24d9c6
CloudRex <cloudrex@outlook.com>
2018-12-21 14:33:31 -0500

    Separate library

--- src/core/stdlib.rx/core/interfaces.ts
+++
@@ -1,7 +0,0 @@
-export interface IDisposable {
-    dispose(): void | any;
-}
-
-export interface IClonable<T> {
-    clone(): T;
-}
--- src/core/stdlib.rx/data-structures/immutable-collection.ts
+++
@@ -1,5 +0,0 @@
-import Collection from "./collection";
-
-export default class ImmutableCollection<TKey, TValue> extends Collection<TKey, TValue> {
-    // TODO
-}
